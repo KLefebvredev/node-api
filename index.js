a63961ce8c1833fcbf0306b9a0c80885c61795e2
klefebvre <klefebvre.dev@gmail.com>
2021-08-20 11:48:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+require('./models/dbConfig');
+const postsRoutes = require('./routes/postsController');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+mongoose.set('useFindAndModify', false)
+
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use('/posts', postsRoutes);
+
+app.listen(5500, () => console.log('Server started: 5500'));
